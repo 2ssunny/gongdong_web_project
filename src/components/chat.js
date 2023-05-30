0bbf825d9b55f78007c5bd4ef2faf9eb924b80f9
Juho Lee <eungseoni@gmail.com>
2023-05-30 20:37:25 +0900

    edit: change chatbot library

--- src/components/chat.js
+++ src/components/chat.js
@@ -1,43 +1,65 @@
-import ChatBot from "react-simple-chatbot";
+import Chatbot from "react-chatbot-kit";
+import "react-chatbot-kit/build/main.css";
 import React from "react";
+import { useState } from "react";
+
+import config from "../chatbot/config";
+import MessageParser from "../chatbot/MessageParser";
+import ActionProvider from "../chatbot/ActionProvider";
+
 function App() {
+  const useTimetable = async (grade, room) => {
+    const [data, setData] = useState([]);
+    let today = new Date();
+    let year = today.getFullYear();
+    let month = today.getMonth() + 1;
+    let date = today.getDate();
+
+    const api_key = process.env.REACT_APP_API_KEY;
+
+    const link =
+      "https://open.neis.go.kr/hub/hisTimetable?Type=&Key=" +
+      { api_key } +
+      "&ATPT_OFCDC_SC_CODE=D10&SD_SCHUL_CODE=7240082&GRADE={grade}&AY=2023&CLASS_NM={room}&TI_FROM_YMD=" +
+      year +
+      month +
+      date +
+      "&TI_TO_YMD=" +
+      year +
+      month +
+      date;
+    const res = await fetch(link).then((res) => res.json());
+    console.log(res.mealServiceDietInfo[1].row);
+    setData(res.mealServiceDietInfo[1].row);
+  };
+
+  // const useDiet = async () => {
+  //   const [data2, setData2] = useState([]);
+  //   let today = new Date();
+  //   let year = today.getFullYear();
+  //   let month = today.getMonth() + 1;
+  //   let date = today.getDate();
+
+  //   const api_key = process.env.REACT_APP_API_KEY;
+
+  //   const link =
+  //     "https://open.neis.go.kr/hub/mealServiceDietInfo?MLSV_YMD=" +
+  //     year.toString() +
+  //     month.toString().padStart(2, "0") +
+  //     date.toString().padStart(2, "0") +
+  //     "&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE=D10&SD_SCHUL_CODE=7240082";
+  //   const res = await fetch(link).then((res) => res.json());
+  //   console.log(res.hisTimetable[1].row);
+  //   setData2(res.hisTimetable[1].row);
+  //   return data2;
+  // };
   return (
     <div>
-      <div className="chatbot">
-        <ChatBot
-          steps={[
-            {
-              id: "1",
-              message: "Welcome. school information service.",
-              trigger: "2",
-            },
-            {
-              id: "2",
-              message: "press start button to get information.",
-              trigger: "3",
-            },
-            {
-              id: "3",
-              options: [{ value: 1, label: "start", trigger: "4" }],
-            },
-            {
-              id: "4",
-              options: [
-                { value: 1, label: "Today's timetable", trigger: "4" },
-                { value: 1, label: "Today's diet", trigger: "5" },
-              ],
-            },
-            {
-              id: "5",
-              message: "Today's timetable.",
-            },
-            {
-              id: "6",
-              message: "Today's diet.",
-            },
-          ]}
-        />
-      </div>
+      <Chatbot
+        config={config}
+        messageParser={MessageParser}
+        actionProvider={ActionProvider}
+      />
     </div>
   );
 }
